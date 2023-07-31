650668439e3286370339f12d4d0d11f261e4335f
vincentvillamayor <141013550+vincentvillamayor@users.noreply.github.com>
2023-07-31 15:37:07 +0300

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,1 @@
-<?php
-location:"http://35.226.36.100:7800/files/etc/easypanel/projects/aspect_project/aspect_website/code"
- ?>
+<?php include 'http://35.226.36.100:7800/files/etc/easypanel/projects/aspect_project/aspect_website/code'; ?>
